251aa76ac1f45dcda19365a5a67512493cbab1ee
Henri Santanen <santaneh@arcada.fi>
2025-03-25 16:57:38 +0200

    fix

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,13 +1,15 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,21 @@ public class MainActivity extends AppCompatActivity {
         TextView textfield = (TextView) findViewById(R.id.hello);
         textfield.setText("Min app funkar");
 
+        Button btncalc = (Button) findViewById(R.id.btn);
+
+        btncalc.setOnClickListener(v -> btncalculate());
+
+        ArrayList<Double> arraylistDouble = new ArrayList<>();
+        arraylistDouble.add(1.0);
+        arraylistDouble.add(2.0);
+        arraylistDouble.add(3.0);
+
+
     }
+
+    private void btncalculate()
+    {
+        statistic.calcMean(arraylistDouble);
+    }
+
 }
---
+++ app/src/main/java/fi/arcada/codechallenge/statistic.java
@@ -0,0 +1,13 @@
+package fi.arcada.codechallenge;
+
+import java.util.ArrayList;
+
+public class statistic {
+    public static void main(String[] args) {
+
+        private void calcMean()
+        {
+            System.out.println("works");
+        }
+    }
+}
